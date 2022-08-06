b0e0dcf926c0948d424f58d1260412b6715843cf
elton <eltonmorenocl@gmail.com>
2022-08-05 21:56:23 -0300

    feat(req3): implementa funcao get_unique_industries - retorna valores unicos da caluna industry desconsiderando valores vazios

--- src/insights.py
+++ src/insights.py
@@ -1,8 +1,8 @@
-import src.jobs as jobs
+from src.jobs import read
 
 
 def get_unique_job_types(path):
-    list_jobs = jobs.read(path)
+    list_jobs = read(path)
     types = set()
     for job in list_jobs:
         types.add(job["job_type"])
@@ -28,21 +28,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    list_jobs = read(path)
+    types = set()
+    for ind in list_jobs:
+        if ind["industry"] != '':
+            types.add(ind["industry"])
+    return types
 
 
 def filter_by_industry(jobs, industry):
