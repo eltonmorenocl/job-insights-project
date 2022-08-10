e3cff51b62a0e4bbe5442a468487489bf218c197
elton <eltonmorenocl@gmail.com>
2022-08-10 11:35:24 -0300

    feat(req5): implementa funcao get_min_salary - retorna valor inteiro com menor salario na coluna min_salary

--- src/insights.py
+++ src/insights.py
@@ -65,21 +65,13 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
+    list_jobs = read(path)
+    salary_min = set()
+    for salary in list_jobs:
+        if salary["min_salary"].isdigit() and salary["min_salary"] != '':
+            salary_min.add(int(salary["min_salary"]))
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    return min(salary_min)
 
 
 def matches_salary_range(job, salary):
